b118aeedc895c088be40e5272ed66703dd921315
Tristan Lenormand <tristan.lenormand@univ-nantes.fr>
2020-11-30 14:57:25 +0100

    final commit

--- M1/src/main/java/M0.java
+++ M1/src/main/java/M0.java
@@ -14,11 +14,22 @@ import Interface.Ports.Ports;
 
 public class M0 {
     public static void main (String[] args){
+
+        // ************* INSTANCIATION DE LA PARTIE CLIENT/SERVER - M1 ********************
+
+        // création de la configuration qui englobe le client et le serveur dans le modèle m1
         ClientServerConfig myConfigurationClientServer = new ClientServerConfig();
+
+        //création du client qui est un composant ainsi que d'un port send_request dans son interface fournis pour se connecter au serveur
 
         Client myClient = new Client();
         ToPort send_request = new ToPort("Send_Request");
         myClient.getInterCompoFrom().getPortsList().add(send_request);
+
+        /*Création du serveur qui est aussi un composant avec un port receive_request dans l'interface requis pour recevoir les requêtes
+        du Client. Le Serveur possède une configuration qui va permettre de décrire les composants qui le compose. Enfin, la configuration
+        du serveur possède un port requis external_socket qui fera le lien avec le connection_manager via un lien binding
+         */
 
         Server myServer = new Server(new Configuration());
         FromPort receive_request = new FromPort("Receive_Request");
@@ -26,6 +37,11 @@ public class M0 {
         FromPortConfig from_external_socket_config = new FromPortConfig("External Socket");
         myServer.getConfig().getInterConfig().getPortsConfig().add(from_external_socket_config);
 
+        /*
+        Création du RPC qui est un connecteur et qui permet de faire le lien entre client et server via un role caller côté interface requis
+        qui est relié au client via un lien, et un role called dans l'interface fournis qui est relié au serveur.
+        Les interfaces sont contenues dans l'attribue glue du connecteur afin qu'elles soient accessible par la glue et par le connecteur.
+         */
 
         RPC rpcConnector = new RPC();
         FromRole caller1 = new FromRole("Caller");
@@ -33,19 +49,36 @@ public class M0 {
         ToRole called1 = new ToRole("Called");
         rpcConnector.getGlue().getToInterface().getRolesList().add(called1);
 
+        /*
+        Création des liens d'attachements entre le client et le rpc et entre le rpc et le serveur. Puis ajout des liens créées à la configuration
+        globale client/serveur car nous avons décidés d'inclure les liens dans la configuration pour qu'ils soient accessibles et qu'un composant
+        puisse via ses ports retrouver à quels autres éléments il est connecté.
+
+         */
+
         Liens clientToRpc = new AttachementFromTo(send_request,caller1);
         Liens rpcToClient = new AttachementToFrom(called1, receive_request);
 
         myConfigurationClientServer.getLinkLists().add(clientToRpc);
         myConfigurationClientServer.getLinkLists().add(rpcToClient);
 
+        // Ajout du client, du rpc et du serveur à la configuration globale
 
         myConfigurationClientServer.getComponentsList().add(myClient);
         myConfigurationClientServer.getComponentsList().add(myServer);
         myConfigurationClientServer.getConnectorsList().add(rpcConnector);
 
-        Connection_Manager myConnectionManager = new Connection_Manager();
+        // ************ FIN DE L'INSTANCIATION DU MODELE M1 **************
 
+        // ************ INTANCIATION DU SERVEUR QUI EST COMPOSITE, MODELE M1_SERVER ****************
+
+        /*
+        Création du premier composant du serveur, le Connection Manager. Il possède deux ports fournis qui permettent de faire la liaison
+        avec les composants Database et Security Manager via des RPC. Il possède également un port fournis external_socket qui est relié
+        à la configuration du Server via un lien binding pour rediriger les requêtes des clients vers le serveur directement au composant
+        Connection Manager
+         */
+        Connection_Manager myConnectionManager = new Connection_Manager();
         ToPort db_query = new ToPort("db_query");
         ToPort security_check = new ToPort("security_check");
         FromPort from_external_socket_cm= new FromPort("External_Socket");
@@ -54,6 +87,11 @@ public class M0 {
         myConnectionManager.getInterCompoTo().getPortsList().add(db_query);
         myConnectionManager.getInterCompoTo().getPortsList().add(security_check);
 
+        /*
+        Création du deuxième composant du serveur : Database. Il possède deux ports requis afin de communiquer avec les deux autres composants
+        Connection Manager et Security Manager via les rpc. Query Interogation est le port qui sera relié à Connection Manager via db_query
+        et security management est le port from qui sera relié au port to queck_query de Security Manager.
+         */
         Database myDatabase = new Database();
 
         FromPort security_management = new FromPort("security_management");
@@ -62,6 +100,11 @@ public class M0 {
         myDatabase.getInterCompoFrom().getPortsList().add(security_management);
         myDatabase.getInterCompoFrom().getPortsList().add(query_interogation);
 
+        /*
+        Création du composant Security Manager avec également deux ports, l'un requis pour se connecter avec le port to de connection manager.
+        Ce port permettra de gérer la gestion de la sécurité des connexions aux serveurs. L'autre est un port fournis pour se connecter au port
+        requis security_management de la database.
+         */
 
         Security_Manager mySecurityMangager = new Security_Manager();
 
@@ -71,7 +114,11 @@ public class M0 {
         mySecurityMangager.getInterCompoTo().getPortsList().add(check_query);
         mySecurityMangager.getInterCompoFrom().getPortsList().add(security_authentification);
 
-
+        /*
+        Création des trois RPC permettant la connection entre les 3 composants du serveur.
+        Chaque RPC a un role requis caller connecté au port fournis d'un composant via un attachement et un role fournis qui est connecté
+        au port requis d'un autre composant via un autre lien d'attachement
+         */
         RPC rpcConnectionManagerDatabase = new RPC();
 
         FromRole caller2 = new FromRole("Caller");
@@ -98,6 +145,7 @@ public class M0 {
         ToRole called4 = new ToRole("Called");
         rpcSecurityManagerDatabase.getGlue().getToInterface().getRolesList().add(called4);
 
+        // Ajout des trois composants et des trois connecteurs RPC à la configuration du serveur
 
         myServer.getConfig().getComponentsList().add(myConnectionManager);
         myServer.getConfig().getComponentsList().add(myDatabase);
@@ -106,6 +154,11 @@ public class M0 {
         myServer.getConfig().getConnectorsList().add(rpcConnectionManagerDatabase);
         myServer.getConfig().getConnectorsList().add(rpcConnectionManagerSecurityManager);
         myServer.getConfig().getConnectorsList().add(rpcSecurityManagerDatabase);
+
+        /*
+        Création des 6 liens d'attachements entre les 3 composants via les roles des 3 rpc.
+        Création du lien binding entre le port requis (From) de la configuration du serveur et le port requis de Connection Manager
+         */
 
         Liens cm_to_sm_rpc = new AttachementFromTo(security_check,caller3);
         Liens rpc_cm_to_sm = new AttachementToFrom(called3,security_authentification);
@@ -118,13 +171,14 @@ public class M0 {
 
         Liens external_socket_binding = new BindingFrom(from_external_socket_cm,from_external_socket_config);
 
+        // Ajout des liens dans la configuration du serveur
+
         myServer.getConfig().getLinkLists().add(cm_to_sm_rpc);
         myServer.getConfig().getLinkLists().add(rpc_cm_to_sm);
         myServer.getConfig().getLinkLists().add(cm_to_db_rpc);
         myServer.getConfig().getLinkLists().add(rpc_cm_to_db);
         myServer.getConfig().getLinkLists().add(sm_to_db_rpc);
         myServer.getConfig().getLinkLists().add(rpc_sm_to_db);
-
         myConfigurationClientServer.getLinkLists().add(external_socket_binding);
 
         System.out.println("M0 architecture implemented");
